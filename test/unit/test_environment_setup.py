e427e0fb65b67ad2b0c2cc1dd9cfd0c618b801a4
Stegallo <stegallo@gmail.com>
2018-12-25 10:46:02 -0800

    Set up of test framework

---
+++ test/unit/test_environment_setup.py
@@ -0,0 +1,19 @@
+"""
+    Test environment for The Bowling Game
+"""
+
+def log(message):
+    """
+        generic log function
+    """
+    print(message)
+
+def init():
+    """
+        set up environment for testing
+    """
+    log("""
+        ... environment setup started ...
+    """)
+
+init()
