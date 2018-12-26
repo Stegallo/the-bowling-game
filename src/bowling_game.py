7a667210780849479e655d1105fe88fb5618732b
Stegallo <stegallo@gmail.com>
2018-12-25 17:27:31 -0800

    Refactor with list

--- src/bowling_game.py
+++ src/bowling_game.py
@@ -7,12 +7,19 @@ class Game(object):
 
     def __init__(self):
         """ init method """
-        self.score = 0
+        # self.score = 0
+        self.rolls = []
+        self.current_roll = 0
 
     def roll(self, pins):
         """ records a roll """
-        self.score += pins
+        # self.score += pins
+        self.rolls.append(pins)
 
     def get_score(self):
         """ calculates final score """
-        return self.score
+        score = 0
+        for i in self.rolls:
+            score = score + i
+
+        return score
