968d761cd469918ffb33a57f4286c5dc0dec5424
Stegallo <stegallo@gmail.com>
2018-12-26 12:25:34 -0800

    Remove comments

--- src/bowling_game.py
+++ src/bowling_game.py
@@ -7,13 +7,11 @@ class Game(object):
 
     def __init__(self):
         """ init method """
-        # self.score = 0
         self.rolls = []
         self.current_roll = 0
 
     def roll(self, pins):
         """ records a roll """
-        # self.score += pins
         self.rolls.append(pins)
 
     def is_spare(self, i):
